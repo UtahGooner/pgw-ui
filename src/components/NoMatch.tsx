e092c20ec48d708f04d2e5bcdb58acff3c129480
Steve Montgomery <steve@chums.com>
2022-10-19 17:02:59 -0600

    feat(404): Updates to the NoMatch component

--- src/components/NoMatch.tsx
+++ src/components/NoMatch.tsx
@@ -1,9 +1,21 @@
 import React from "react";
+import {Figure} from "react-bootstrap";
+import {Helmet} from "react-helmet";
 
 const NoMatch = () => {
     return (
-        <div className="container-md py-5">
+        <div className="container py-5">
+            <Helmet>
+                <title>404: Not Found</title>
+            </Helmet>
             <h1>The page your are looking for was not found.</h1>
+            <Figure>
+                <Figure.Image width={400} height={266} alt="404, Not found!"
+                              src="/images/smithsonian-butte-ut/800/img_8504.jpeg" />
+                <Figure.Caption>
+                    You seem to have arrived at a non-existent page.
+                </Figure.Caption>
+            </Figure>
         </div>
 
     )
